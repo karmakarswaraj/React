1e1683a31d17159b4db68893db35b050d859eb9d
karmakarswaraj <swarajkarmakar5@gmail.com>
2024-02-27 22:00:13 +0530

    Final

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,40 +1,49 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
-  const [numAllowed, setNumAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
+  const [numAllowed, setNumAllowed] = useState(true);
+  const [charAllowed, setCharAllowed] = useState(true);
   const [password, setPassword] = useState("");
 
   const passwordGenerator = useCallback(() => {
-    let pass = " ";
+    let pass = "";
     let str =
-      "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numAllowed) str += "0123456789";
     if (charAllowed) str += "@#$&*!?:";
-    for (let i = 0; i <= length; i++) {
-      let char = math.floor(math.random * str.length + 1);
-      pass = str.charAt(char);
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numAllowed, charAllowed, setPassword]);
+  useEffect(() => {
+    passwordGenerator()
+  },[length, numAllowed, charAllowed, passwordGenerator]);
+  
   return (
     <>
       <h1 className="text-3xl text-center ">Password Generator</h1>
       <br />
-      <div className="bg-blue-800 p-4 rounded-lg ">
-        <div className="flex justify-center p-4 m-4 gap-4">
-          <input value="{password}"className="w-full p-2 rounded-lg" type="text" readOnly/>
+      <div className="w-full p-4 bg-blue-800 rounded-lg">
+        <div className="flex justify-center gap-4 p-4 m-4">
+          <input placeholder="Password" value={password} className="w-full p-2 rounded-lg" type="text" readOnly/>
           <button>Copy</button>
         </div>
-        <div className="flex justify-between p-4 m-4 gap-4">
-          <input type="range" id="slider" name="slider" min="0" max="100" value="50" />
-          <label for="slider">length({length})</label>
-          <input type="checkbox" id="checkbox" name="checkbox" />
-          <label for="slider">Number</label>
-          <input type="checkbox" id="checkbox" name="checkbox" />
-          <label for="slider">Charecter</label>
+        <div className="flex justify-between gap-4 p-4 m-4">
+          <input type="range" min={8} max={100} value={length} className="cursor-pointer" onChange={(e) => {
+            setLength(e.target.value)
+          }}/>
+          <label>length: {length}</label>
+          
+          <input type="checkbox" defaultChecked={numAllowed} onChange={() => {setNumAllowed((prev) => !prev)}}/>
+          <label>Number</label>
+          
+          <input type="checkbox" defaultChecked={charAllowed} onChange={() => {setCharAllowed((prev) => !prev)}}/>
+          <label>Charecter</label>
+          
         </div>
       </div>
     </>
