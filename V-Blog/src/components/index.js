cdd4f3c92702a86b0a9d69a2392f50921348e9d6
karmakarswaraj <swarajkarmakar5@gmail.com>
2024-03-12 02:13:57 +0530

    Final commit

--- V-Blog/src/components/index.js
+++ V-Blog/src/components/index.js
@@ -1,7 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
-import Logo from "./Logo";
 import LogoutBtn from "./Header/Logout";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
 
-export { Header, Footer, Container ,LogoutBtn, Logo}
+export { Header, Footer, Container, LogoutBtn, Logo, Button, Input };
